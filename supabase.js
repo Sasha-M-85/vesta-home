29d7a72f19b82332a17431bc875ebb6cb27967d5
Sasha-M-85 <sasha.m.85@gmail.com>
2025-07-27 20:34:46 +0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from './supabase.min.js';
-
-const supabaseUrl = 'https://rincxoalnhfsnwgwxrfb.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpbmN4b2Fsbmhmc253Z3d4cmZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MjE2MzMsImV4cCI6MjA2OTE5NzYzM30.95JhUW9uRbcHSkFCXyh40acSAAnJIj3nbUICQGqW32Y';
-
-window.supabase = createClient(supabaseUrl, supabaseKey);
