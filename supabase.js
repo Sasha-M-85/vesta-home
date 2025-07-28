6aed410df506b41e688843b56eedde9ea75d7a47
Sasha-M-85 <sasha.m.85@gmail.com>
2025-07-28 23:44:34 +0300

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,3 +1,7 @@
 
-const { createClient } = supabase;
-const supabaseClient = createClient('https://rincxoalnhfsnwgwxrfb.supabase.co', 'public-anon-key');
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+const supabaseUrl = 'https://rincxoalnhfsnwgwxrfb.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpbmN4b2Fsbmhmc253Z3d4cmZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MjE2MzMsImV4cCI6MjA2OTE5NzYzM30.95JhUW9uRbcHSkFCXyh40acSAAnJIj3nbUICQGqW32Y';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
