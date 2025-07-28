fda1d3fa79385752968ed630eb0445b2b3c11dde
Sasha-M-85 <sasha.m.85@gmail.com>
2025-07-28 23:59:35 +0300

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,3 +1,3 @@
-const supabaseUrl = 'https://rincxoalnhfsnwgwxrfb.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpbmN4b2Fsbmhmc253Z3d4cmZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MjE2MzMsImV4cCI6MjA2OTE5NzYzM30.95JhUW9uRbcHSkFCXyh40acSAAnJIj3nbUICQGqW32Y';
-const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+
+const { createClient } = supabase;
+const supabaseClient = createClient('https://rincxoalnhfsnwgwxrfb.supabase.co', 'public-anon-key');
